54398568149da837ad08b6d0ad0baa9bbf8ac9ea
jleslie17 <jleslie17@yahoo.com>
2017-04-01 09:07:25 +0100

    Tidying up the directory, mostly

--- Project/R/VR_2017.R
+++ Project/R/VR_2017.R
@@ -2,7 +2,7 @@ library(lsa)
 library(tidyverse)
 
 path_to_scripts <- file.path("project/src")
-datadir <- file.path("/project/data")
+datadir <- file.path("project/data")
 codedir <- file.path("/project/code")
 Rdir <- file.path("/project/R")
 
@@ -12,28 +12,31 @@ source(file.path(path_to_scripts, "SpreadResponses.R"))
 source(file.path(path_to_scripts, "GetMatches.R"))
 source(file.path(path_to_scripts, "CompanyMatchesOutput.R"))
 
-# Load data
-del_data <- read.csv(
+# Load data ====
+delegate_data <- read.csv(
   "project/data/VR_Data_010217.csv",
   header = T,
   na.strings = '')
-head(del_data)
+head(delegate_data)
 
 # del_tibble <- read_csv(
 #   "project/data/VR_Data_010217.csv"
 # )
 
-sponsor_data <- read.xlsx(paste(datadir, "Sponsor Intel 6 March.xlsx", sep = ''),
-                         sheetIndex = 1,
-                         header = T)
+sponsor_file <- "Copy of Sponsor Intel 6 March new.xlsx"
+sponsor_data <- read.xlsx(file.path(datadir, sponsor_file), 
+                          sheetIndex = 1,
+                          header = T)
 # Clean data ====
 # Empty rows in del_data? No!
-nrow(del_data[rowSums(is.na(del_data)) == ncol(del_data),])
+nrow(delegate_data[rowSums(is.na(delegate_data)) == ncol(delegate_data),])
 # nrow(del_tibble[rowSums(is.na(del_tibble)) == ncol(del_tibble),])
 nrow(sponsor_data[rowSums(is.na(sponsor_data)) == ncol(sponsor_data),])
 
-targets <- del_data[, c(12:19)]
-users <- sponsor_data[, c(5:6)]
+targets <- delegate_data[, c(12:19)]
+
+names(sponsor_data)
+users <- sponsor_data[, 5:10]
 
 # targets2 <- del_tibble %>% 
 #   select(c(12:19))
@@ -50,3 +53,25 @@ users <- AddUnderscores(users)
 head(users)
 usersSpread <- SpreadResponses(users)
 names(usersSpread)
+
+sort(names(tarSpread))
+sort(names(usersSpread))
+table(names(tarSpread) %in% names(usersSpread))
+table(names(usersSpread) %in% names(tarSpread))
+names(usersSpread[!names(usersSpread) %in% names(tarSpread)])
+names(tarSpread[!names(tarSpread) %in% names(usersSpread)])
+
+# Figure out where the matches go ====
+head(delegate_data)
+names(delegate_data)
+levels(delegate_data$I.Am.An.End.User.With.An.Interest.In.The.Following.VR.AR.MR.Applications.)
+levels(delegate_data$I.Represent.A.Technology.Provider.Best.Described.As.The.Following.)
+levels(delegate_data$Which.technology.is.of.most.interest.to.you.)
+levels(delegate_data$I.Would.Like.To.Learn.More.About.The.Following.VR.AR.MR.Opportunities.At.The.Show.)
+t19 <- SpreadResponses(
+  as.data.frame(delegate_data$I.Would.Like.To.Learn.More.About.The.Following.VR.AR.MR.Opportunities.At.The.Show.))
+names(t19)
+t19 <- AddUnderscoresPipes(
+  as.data.frame(delegate_data$I.Would.Like.To.Learn.More.About.The.Following.VR.AR.MR.Opportunities.At.The.Show.)) %>% 
+  SpreadResponses()
+names(t19)
